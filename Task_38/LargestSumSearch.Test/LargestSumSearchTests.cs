06f1899d7788b4c63890bd7f6ea7dcc2ffce021e
Nikiforov <nikiforov.aleksandr@gmail.com>
2017-08-01 15:37:45 +0300

    38 update

--- Task_38/LargestSumSearch.Test/LargestSumSearchTests.cs
+++ Task_38/LargestSumSearch.Test/LargestSumSearchTests.cs
@@ -12,14 +12,63 @@ namespace LargestSumSearch.Test
     public class LargestSumSearchTests
     {
         [TestMethod]
-        public void CorrectArrayTest()
+        public void ArrayIsNotEmptyTest()
         {
-            int[] arr = { -2, -3, -4, 1, -2, -1, -5, -3 };
+            int[] arr = { -2, -3, -4, 1, 2, -1, -5, -3 };
 
             int[] result = Task_38.LargestSumSearch.FindLargestSum(arr);
 
-            CollectionAssert.AreEqual(new []{ 4, -1, -2, 1, 5 }, result);
+            Assert.IsTrue(result.Length > 0);
+        }
 
+        [TestMethod]
+        public void ArrayIsCorrectTest()
+        {
+            int[] arr = { -1, 4, -2, 5, -5, 2, -20, 6 };
+
+            int[] result = Task_38.LargestSumSearch.FindLargestSum(arr);
+
+            CollectionAssert.AreEqual(new[] { 4, -2, 5 }, result);
+        }
+
+        [TestMethod]
+        public void ArrayAllNegativeTest()
+        {
+            int[] arr = { -11, -4, -2, -5, -5, -2, -20, -6 };
+
+            int[] result = Task_38.LargestSumSearch.FindLargestSum(arr);
+
+            CollectionAssert.AreEqual(new[] { -2 }, result);
+        }
+
+        [TestMethod]
+        public void ArrayAllPositiveTest()
+        {
+            int[] arr = { 11, 4, 2, 5, 5, 2, 20, 6 };
+
+            int[] result = Task_38.LargestSumSearch.FindLargestSum(arr);
+
+            CollectionAssert.AreEqual(new[] { 11, 4, 2, 5, 5, 2, 20, 6 }, result);
+        }
+
+        [TestMethod]
+        public void ArrayIsEmptyTest()
+        {
+            int[] arr = new int[0];
+
+            int[] result = Task_38.LargestSumSearch.FindLargestSum(arr);
+
+            CollectionAssert.AreEqual(new int[0], result);
+        }
+
+        [TestMethod]
+        public void ArrayOneZeroElementTest()
+        {
+            int[] arr = {0};
+
+            int[] result = Task_38.LargestSumSearch.FindLargestSum(arr);
+
+            CollectionAssert.AreEqual(new []{0}, result);
         }
     }
 }
