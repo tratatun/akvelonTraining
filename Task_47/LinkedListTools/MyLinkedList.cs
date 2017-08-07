36c18e5c5686ee854943e9ebccbb79609822e733
Nikiforov <nikiforov.aleksandr@gmail.com>
2017-08-08 00:58:35 +0300

    47

---
+++ Task_47/LinkedListTools/MyLinkedList.cs
@@ -0,0 +1,65 @@
+﻿namespace LinkedListTools
+{
+    public class MyLinkedList
+    {
+        public int Data;
+        public MyLinkedList Child;
+
+        public MyLinkedList()
+        {
+        }
+
+        public MyLinkedList(int data, MyLinkedList child = null)
+        {
+            Data = data;
+            Child = child;
+        }
+
+        public void AddLast(MyLinkedList last)
+        {
+            if (Child != null)
+            {
+                Child.AddLast(last);
+            }
+            else
+            {
+                Child = last;
+            }
+        }
+
+        public void AddAtIndex(MyLinkedList node, int index)
+        {
+            if (index <= 0)
+            {
+                node.Child = Child;
+                Child = node;
+            }
+            else
+            {
+                index--;
+                Child.AddAtIndex(node, index);
+            }
+        }
+
+        public void DeleteElements(int indexStart, int indexlast)
+        {
+            //if (index <= 0)
+            //{
+            //    node.Child = Child;
+            //    Child = node;
+            //}
+            //else
+            //{
+            //    index--;
+            //    Child.AddAtIndex(node, index);
+            //}
+        }
+
+
+
+        public override string ToString()
+        {
+            return Data + "->" + Child;
+        }
+    }
+}
