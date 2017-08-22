cbc66273144c85c62af25ac0555e3f9cb93f7c86
Nikiforov <nikiforov.aleksandr@gmail.com>
2017-08-23 00:52:58 +0300

    46 initial

---
+++ First/Task_46/FastMultiplier/FastMultiplier.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FastMultiplier
+{
+    public static class FastMultiplier
+    {
+        public static int FastMultiplierTo7(int num)
+        {
+            int result = 0;
+
+            return result;
+        }
+    }
+}
