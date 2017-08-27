a60d34806bbbec0fb493faf1a0a52ae3a8f64cb3
Nikiforov <nikiforov.aleksandr@gmail.com>
2017-08-27 22:17:06 +0300

    51 initial

---
+++ First/Task_51/StringExcluder_starter/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringExcluder_starter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(StringExcluder.StringExcluder.ExludeCharacters("ad", "qweasdf"));
+            Console.ReadKey();
+        }
+    }
+}
