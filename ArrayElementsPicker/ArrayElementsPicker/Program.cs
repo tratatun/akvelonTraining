0a4a0ec98cd575626cdb168d07a847fa93e6bc5a
Nikiforov <nikiforov.aleksandr@gmail.com>
2017-09-21 00:52:54 +0300

    ArrayElementsPicker

---
+++ ArrayElementsPicker/ArrayElementsPicker/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+/*
+ Given an Array A with n elements. Pick maximum number of elements from given array following the rule:  We cannot pick A[i] and A[j] if absolute value of (A[i] - A[j]) > absolute value of (i - j) 
+
+Example: {13,5,4} 
+Ans: 2 
+Pick 5 and 4.
+*/
+namespace ArrayElementsPicker
+{
+    public class Program
+    {
+        public static void Main(string [] args)
+        {
+            string[] strarr = Console.ReadLine().Split(' ');
+            int[] arr = new int[strarr.Length]; //
+            
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = int.Parse(strarr[i]);
+            }
+
+            int[][]res = ArrayElementsPicker.PickElemByRule(arr);
+
+        }
+
+    }
+
+    public static class ArrayElementsPicker
+    {
+        public static int[][] PickElemByRule(int[] arr)
+        {
+            int[][] result = new int[arr.Length * arr.Length][],clearResult;
+            int k = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    int sum = arr[i] - arr[j], indSum = i - j;
+                    sum = sum < 0 ? -sum : sum;
+                    indSum = indSum < 0 ? -indSum : indSum;
+
+                    if (sum <= indSum)
+                    {
+                        result[k] = new int[2];
+                        result[k][0] = arr[i];
+                        result[k++][1] = arr[j];
+                    }
+                }
+            }
+            clearResult=new int[k][];
+            for (int i = 0; i < k; i++)
+            {
+                clearResult[i] = result[i];
+            }
+
+            return clearResult;
+        }
+
+    }
+}
---
+++ ArrayElementsPicker/UnitTestProject1/ArrayElementsPickerTests.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ArrayElementsPickerTests
+{
+    [TestClass]
+    public class ArrayElementsPickerTests
+    {
+        [TestMethod]
+        public void EmptyListTest()
+        {
+            int[] arr = { };
+
+             int [][] res = ArrayElementsPicker.ArrayElementsPicker.PickElemByRule(arr);
+
+            CollectionAssert.AreEqual(res, new int[0][]);
+        }
+
+        [TestMethod]
+        public void OneElemTest()
+        {
+            int[] arr = { 4 };
+
+            int[][] res = ArrayElementsPicker.ArrayElementsPicker.PickElemByRule(arr);
+
+            CollectionAssert.AreEqual(res, new int[0][]);
+        }
+
+        [TestMethod]
+        public void RegularTest()
+        {
+            int[] arr = { 13, 5, 4 };
+
+            int[][] res = ArrayElementsPicker.ArrayElementsPicker.PickElemByRule(arr);
+
+            int[][] t = new[] {new[] {5, 4}};
+            for (int i =0;i<res.Length;i++)
+            {
+                CollectionAssert.AreEquivalent(t[i], res[i]);
+
+            }
+        }
+
+        [TestMethod]
+        public void MultipleSameTest()
+        {
+            int[] arr = {1, 1, 1, 1, 1, 1};
+
+            int[][] res = ArrayElementsPicker.ArrayElementsPicker.PickElemByRule(arr);
+
+            int[][] t = new[]
+            {
+                new int[] {1, 1},
+                new int[] {1, 1},
+                new int[] {1, 1},
+                new int[] {1, 1},
+                new int[] {1, 1},
+                new int[] {1, 1},
+                new int[] {1, 1},
+                new int[] {1, 1},
+                new int[] {1, 1},
+                new int[] {1, 1},
+                new int[] {1, 1},
+                new int[] {1, 1},
+                new int[] {1, 1},
+                new int[] {1, 1},
+                new int[] {1, 1},
+                new int[] {1, 1},
+                
+            };
+            for (int i = 0; i < res.Length; i++)
+            {
+                CollectionAssert.AreEquivalent(t[i], res[i]);
+
+            }
+        }
+    }
+}
