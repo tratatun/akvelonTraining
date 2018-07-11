95837f4447a680b6c819f52aa79e407c5e6eb130
nikiforov.aleksandr <nikiforov.aleksandr@gmail.com>
2018-07-11 13:05:44 +0300

    check If Leaves At the Same Level

---
+++ interviews/LeavesOnSameLevel/LeavesOnSameLevel/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeavesOnSameLevel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ////Check if all leaves are at same level
+            /// Recursive
+            //int n = int.Parse(Console.ReadLine());
+            Tree root = null, current = root;
+            root = new Tree(0);
+            root.Left = new Tree(1);
+            root.Right = new Tree(2);
+            root.Left.Left = new Tree(3);
+            root.Left.Right = new Tree(4);
+
+            root.Right.Left = new Tree(5);
+            root.Right.Right = new Tree(6);
+
+            root.Right.Right.Left = new Tree(7);
+            bool isTheSame = root.IsLeavesAtSameLevel();
+            Console.WriteLine(isTheSame);
+        }
+    }
+
+    public class Tree
+    {
+        public Tree Left { get; set; }
+        public Tree Right { get; set; }
+        public int Data { get; set; }
+        public Tree(int data)
+        {
+            Data = data;
+        }
+
+        public bool IsLeavesAtSameLevel()
+        {
+            int level = 0;
+            return CheckIsLeavesAtSameLevel(this, ref level, 0);
+        }
+        private static bool CheckIsLeavesAtSameLevel(Tree root, ref int level, int current)
+        {
+            if(root == null)
+            {
+                return true;
+            }
+            if (root.Left == null && root.Right == null)
+            {
+                if (level == 0)
+                {
+                    level = current;
+                    return true;
+                }
+
+                return level == current;
+            }
+
+            return CheckIsLeavesAtSameLevel(root.Left, ref level, current + 1) && CheckIsLeavesAtSameLevel(root.Right, ref level, current + 1);
+        }
+    }
+}
---
+++ interviews/LeavesOnSameLevel/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeavesOnSameLevel;
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        [ExpectedException(typeof(NullReferenceException))]
+        public void NullTest()
+        {
+            //assign
+            Tree root = null;
+            int level = 0;
+            //act
+            
+            bool isTheSameLevel = root.IsLeavesAtSameLevel();
+            //assert
+        }
+
+        [TestMethod]
+        public void SameLevelTest()
+        {
+            //assign
+            Tree root = new Tree(1);
+            root.Left = new Tree(2);
+            root.Right = new Tree(3);
+
+            root.Left.Left = new Tree(4);
+            root.Left.Left.Left = new Tree(55);
+            root.Right.Left = new Tree(8);
+            root.Right.Left.Left = new Tree(77);
+            //act
+            bool result = root.IsLeavesAtSameLevel();
+
+            //assert
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void DifferentLevelTest()
+        {
+            //assign
+            Tree root = new Tree(1);
+            root.Left = new Tree(2);
+            root.Right = new Tree(3);
+
+            root.Left.Left = new Tree(4);
+            root.Left.Left.Left = new Tree(55);
+            root.Right.Left = new Tree(8);
+            root.Right.Left.Left = new Tree(77);
+            root.Right.Left.Left.Left = new Tree(44);
+            //act
+            bool result = root.IsLeavesAtSameLevel();
+
+            //assert
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void OneLevelTest()
+        {
+            //assign
+            Tree root = new Tree(1);
+            //act
+            bool result = root.IsLeavesAtSameLevel();
+
+            //assert
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void LevelTest()
+        {
+            //assign
+            Tree root = new Tree(1);
+            root.Left = new Tree(2);
+            root.Right = new Tree(3);
+
+            root.Left.Left = new Tree(4);
+            root.Left.Left.Left = new Tree(55);
+            root.Right.Left = new Tree(8);
+            root.Right.Left.Left = new Tree(77);
+            root.Right.Left.Left.Left = new Tree(44);
+            //act
+            bool result = root.IsLeavesAtSameLevel();
+
+            //assert
+            Assert.IsFalse(result);
+        }
+
+    }
+}
